e02a1a32a783601c88934dc9b77fd86a00c9522c
tddgit096-pc <>
2025-11-21 18:10:07 -0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+// Lab 34 | Toma Dimov | COMSC 210 
+////STEP 1
+//through the prompt engineering, transform the code by adding DFS and BFS
+//write the driver program to excercies code
+//commit: "Step 1 Complete"
+
+/////STEP 2
+//Change the graph by deletign at least two nodes and adding at least six nodes, change the weights as well.
+//Step 2 complete commit
+
+///STEP 3
+//Through expert prompt engineering, wrap an application around this new graph. 
+//It should be some real-world example that is represented by your graph.
+
+//STEP 4
+//Through prompt engineering, add code to procide the shortest path through your graph.
+//Ste4 complete
+
+//STEP 5
+//Through prompt engineering, add code to provide the minimum spanning tree of your graph.
+
+//Step 6
+//Place a menu with all funcionality.
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+}
