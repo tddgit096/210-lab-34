0030011d7e45dddff31845d338ae2066421219d9
tddgit096-pc <>
2025-11-23 18:29:04 -0800

    taking break.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,4 @@
 // Lab 34 | Toma Dimov | COMSC 210 
-
-/////STEP 2
-//Change the graph by deletign at least two nodes and adding at least six nodes, change the weights as well.
-//Step 2 complete commit
-
-///STEP 3
-//Through expert prompt engineering, wrap an application around this new graph. 
-//It should be some real-world example that is represented by your graph.
-
-//STEP 4
-//Through prompt engineering, add code to procide the shortest path through your graph.
-//Ste4 complete
-
-//STEP 5
-//Through prompt engineering, add code to provide the minimum spanning tree of your graph.
-
-//Step 6
-//Place a menu with all funcionality.
 #include <iomanip>
 #include <iostream>
 #include <vector>
